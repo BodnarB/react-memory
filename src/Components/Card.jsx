fd599bef02bb3f005051254ccfb44473d5cfc729
Boglarka Bodnar <bodnarbog94@gmail.com>
2022-12-02 14:24:34 +0100

    up flip function

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,29 +1,15 @@
 import React from 'react'
 import cardBG from '../cardbg.jpg'
 
-export default function Card({ cardFace, flipFunc, order }) {
-
-
-    // const [cardPicture, setcardPicture] = useState([])
-    // const isMount = useRef(true)
-
-    // useEffect(() => {
-    //     if (isMount.current) {
-    //         isMount.current = false
-    //         fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-    //             .then(res => res.json())
-    //             .then(food => setcardPicture(food.meals[0]))
-    //         console.log('use effect lefut')
-    //     }
-    // })
+export default function Card({ cardFace, flipFunc }) {
 
     return (
         <>
-            <div style={order} onClick={flipFunc} className='card-container'>
+            <div onClick={flipFunc} className='card-container'>
                 <img className='card-bg' src={cardBG} alt="" />
                 <img className='card-face hide' src={cardFace} alt="" />
             </div >
-            <div style={order} onClick={flipFunc} className='card-container'>
+            <div onClick={flipFunc} className='card-container'>
                 <img className='card-bg' src={cardBG} alt="" />
                 <img className='card-face hide' src={cardFace} alt="" />
             </div >
--- src/Components/CardTable.jsx
+++ src/Components/CardTable.jsx
@@ -9,41 +9,64 @@ export default function CardTable() {
         'medium': 12,
         'hard': 16
     }
-
     const [difficulty, setDifficulty] = useState('easy')
     const [cardImgs, setCardImgs] = useState([])
-
+    const fetchTimes = useRef(0)
+    let tableSize = tableDifficulty[difficulty]
+    let currentCards = []
+    let firstCard = null
 
     function handleSelect(e) {
         setDifficulty(e.target.value)
         fetchTimes.current = 0
         setCardImgs([])
+        resetCards()
     }
-
-    let tableSize = tableDifficulty[difficulty]
 
     useEffect(() => {
         let cards = document.querySelectorAll('.card-container')
         cards.forEach(card => card.style.order = Math.floor(Math.random() * cards.length))
-    },[tableSize])
+    }, [tableSize])
 
-
-    const fetchTimes = useRef(0)
     useEffect(() => {
-        console.log('use effect lefutott')
-        async function fetchImg() {
-            while (fetchTimes.current !== tableSize / 2) {
-                fetchTimes.current += 1
-                let res = await fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-                let meal = await res.json()
-                setCardImgs(current => [...current, meal.meals[0].strMealThumb])
-                console.log('async fetchImg lefutott')
-            }
-            
+        console.log('use effect fetch nelkul', fetchTimes, tableSize / 2)
+        while (fetchTimes.current !== tableSize / 2) {
+            fetchTimes.current += 1
+            fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+                .then(res => res.json())
+                .then(meal => {
+                    setCardImgs(prevImgs => [...prevImgs, meal.meals[0].strMealThumb])
+                })
+            console.log('fetch lefut')
         }
-        fetchImg()
-    },[tableSize])
+    }, [tableSize])
 
+    function resetCards() {
+        let foundCards = document.querySelectorAll('.found')
+        foundCards.forEach(card => card.classList.toggle('hide'))
+        foundCards.forEach(card => card.classList.remove('found'))
+    }
+
+    function flipCard(e) {
+        let card = Array.from(e.target.parentNode.children)
+        if (!e.target.classList.contains('found') && currentCards.length < 4 && e.target.parentNode !== firstCard) {
+            currentCards.push(...e.target.parentNode.children)
+            card.forEach(element => element.classList.toggle('hide'));
+        }
+        if (currentCards.length === 2) {
+            firstCard = e.target.parentNode
+        }
+        if (currentCards.length === 4 && currentCards[1].src !== currentCards[3].src) {
+            setTimeout(() => {
+                currentCards.forEach(card => card.classList.toggle('hide'))
+                currentCards = []
+            }, 300)
+        }
+        else if (currentCards.length === 4 && currentCards[1].src === currentCards[3].src) {
+            currentCards.forEach(card => card.classList.add('found'))
+            currentCards = []
+        }
+    }
 
 
     return (
@@ -51,11 +74,8 @@ export default function CardTable() {
             <InputSelect difficulty={difficulty} handleSelect={handleSelect} />
             <div>Difficulty: {difficulty} ({tableSize})</div>
             <div className='card-table'>
-                {[...Array(tableSize / 2)].map((element, index) => <Card key={index} cardFace={`${cardImgs[index]}`} />)}
+                {[...Array(tableSize / 2)].map((element, index) => <Card key={index} cardFace={`${cardImgs[index]}`} flipFunc={flipCard} />)}
             </div>
-
         </>
     )
 }
-
-//cardFace={`${cardImgs[index].strMealThumb}`} 
